d3671dd8c2e2fe4895987327a66ca3d7b0ee50f0
Mike Luz <mikeluz@Mikes-MacBook-Pro.local>
2023-05-18 18:45:23 -0400

    feat: pseudocode plan

--- src/ui/app.js
+++ src/ui/app.js
@@ -38,22 +38,22 @@ export default class App extends React.Component {
      * 2. Pass those names to the Load component
      * 
      * Load Component: 
-     * 3. Use the folder names to etch the files for each folder
-     *    - concat into array or keep separate like Map<folderName, files[]>
+     * 3. Use the folder names to fetch the files for each folder.
+     *    - concat into array OR keep separate like Map<folderName, files[]>
      * 4. Save playlist (many songs, many users), folders (many songs, many users), and songs (many folders, many playlists) in db
      *    - We many have to start small here and start with one user per playlist,
-     *      but it would be cool to have many users, so have the ability to share a playlist with another user
-     *      that would accept a gmail (the person you want to share with) and then automatically given that gmail access to the files
-     *      and add the newUser to Playlist relation in the db.
-     * 5. Render Playlist component and display songs for this playlist
+     *      but it would be cool eventually to have many users, so have the ability to share a playlist with another user, form
+     *      that would accept a gmail (the person you want to share with) and then automatically give that gmail access to the files
+     *      and add the new User to Playlist relation in the db.
+     * 5. Render Playlist component and display Songs for this playlist.
      * 6. Allow user to play, etc.
      * 
      * 
      * App (this) component:
-     * 1. After Login, present options list: Load, View (Playlists or Songs), Shuffle (create randomized playlist of all songs the app knows about for this user)
+     * 1. After Login, first present options list: Load, View (Playlists or Songs), Shuffle (create randomized playlist of all songs the app knows about for this user)
      * 
      * Playlist component:
-     * 1. Need an audio player with play, pause, stop, track change, track jump, repeat, loop (loop sections of song you can set via the progress bar), shuffle but 
+     * 1. Need an audio player with play, pause, stop, track change, track jump, repeat, loop (loop sections of song you can set via the progress bar), shuffle (but 
      *    that should be integrated with the same Shuffle button), volume control, progress slider/skipper.
      * 2. Play component needs to load the track from google (drive API call to get file with alt: 'media' option)
      *    and should use Web Audio component to play.
