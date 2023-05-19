a4d8c74ddbceea2e10243a6af5a278073b02781e
Mike Luz <mikeluz@Mikes-MacBook-Pro.local>
2023-05-19 13:01:58 -0400

    feat: adds load folder name input with input handler

--- src/ui/components/load.js
+++ src/ui/components/load.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import TextField from '@mui/material/TextField';
 import { 
 	getAccessToken, 
 	fetchDriveFolders, 
@@ -8,10 +9,11 @@ import {buttonStyle} from '../styles/styles.js';
 
 const Load = () => {
 	const [isHovering, setIsHovering] = useState(false);
-	
+	const [folderName, setFolderName] = useState('');
+
 	const handleClick = () => {
 		getAccessToken(async (token) => {
-			const folders = await fetchDriveFolders('demos', token);
+			const folders = await fetchDriveFolders(folderName, token);
 			console.log('folders', folders);
 			const files = await fetchDriveFolderContents(folders, token);
 			console.log(files);
@@ -27,12 +29,15 @@ const Load = () => {
 	};
 
 	return (
-		<button 
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-			onClick={handleClick} 
-			style={buttonStyle(isHovering)}
-		>Load</button>
+		<div>
+			<TextField id="folder-name" label="Enter folder name" variant="outlined" onChange={(evt) => setFolderName(evt.target.value)} />
+			<button 
+		        onMouseEnter={handleMouseEnter}
+		        onMouseLeave={handleMouseLeave}
+				onClick={handleClick} 
+				style={buttonStyle(isHovering)}
+			>Load</button>
+		</div>
 	)
 }
 
