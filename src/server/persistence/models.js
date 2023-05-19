70f5f4a6bdbc7e3123db77198a0f70901b1017bd
Mat Lundin <matlundin.dev@gmail.com>
2023-05-18 23:42:00 -0500

    add db models

--- src/server/persistence/models.js
+++ src/server/persistence/models.js
@@ -1,11 +1,43 @@
-const {DataTypes} = require('sequelize');
-const {pg} = require('./db.js');
+const { DataTypes } = require('sequelize');
+const { pg } = require('./db.js');
 
 const User = pg.define('User', {
+  id: DataTypes.INTEGER,
   username: DataTypes.STRING,
+  password: DataTypes.STRING,
   birthday: DataTypes.DATE,
+});
+
+const Song = pg.define('Song', {
+  id: DataTypes.INTEGER,
+  artist: DataTypes.STRING,
+  title: DataTypes.STRING,
+  size: DataTypes.STRING
+});
+
+const LoadedSongs = pg.define('LoadedSongs', {
+  songs: { 
+    type: DataTypes.ARRAY,
+    references: {
+    model: {tableName: Song},
+    key: 'id'
+  }}
+});
+
+const Playlist = pg.define('Playlist', {
+  name: DataTypes.STRING,
+  songs: { 
+    type: DataTypes.ARRAY,
+    references: {
+    model: {tableName: Song},
+    key: 'id'
+  }},
 });
 
 module.exports = {
   User,
+  Song,
+  LoadedSongs,
+  Playlist
+
 };
