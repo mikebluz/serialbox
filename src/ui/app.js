183ecc1cad9bf8fbd68d4c4f7027e54a9738276e
Mike Luz <mikeluz@Mikes-MacBook-Pro.local>
2023-05-22 11:50:01 -0400

    feat: player refactor

--- src/ui/app.js
+++ src/ui/app.js
@@ -1,18 +1,30 @@
 import axios from 'axios';
-import { useState, useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
-// mui
+// mui components
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Link from '@mui/material/Link';
+import Slider from '@mui/material/Slider';
+import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
+// mui icons
+import VolumeDown from '@mui/icons-material/VolumeDown';
+import VolumeUp from '@mui/icons-material/VolumeUp';
+
 // repo
-import { initGapi, initGapiTokenClient, parseJwt } from './api/gapi.js';
+import {
+  getAccessToken,
+  fetchDriveFileBlob, 
+  initGapi, 
+  initGapiTokenClient, 
+  parseJwt 
+} from './api/gapi.js';
 import {
   buttonStyle, 
   componentDisplayStyle, 
@@ -23,22 +35,39 @@ import {
 import AllSongs from './components/allsongs.js';
 import Playlist from './components/playlist.js';
 import Playlists from './components/playlists.js';
-import Player from './components/player.js';
+import PlayerControls from './components/playercontrols.js';
 import Load from './components/load.js';
 import Shuffle from './components/shuffle.js';
 
 const CLIENT_ID = `${process.env.REACT_APP_GAPI_CLIENT_ID}.apps.googleusercontent.com`;
 
 const App = (props) => {
+
   const [songsLoaded, setSongsLoaded] = useState([]);
-  const [trackRefData, setTrackRefData] = useState(undefined);
+  const [songsAreLoaded, setSongsAreLoaded] = useState(false);
+  // "setIsPlaying" precipitates the playing
+  const [isPlaying, setIsPlaying] = useState(false);
   const [src, setSrc] = useState('');
+  const [trackLoaded, setTrackLoaded] = useState(false);
+  const [trackIndex, setTrackIndex] = useState(0);
+  const [volume, setVolume] = useState(0.5);
 
   const trackRef = useRef();
 
   const handleLoadedSongs = (songs) => {
-    console.log("songs loaded")
-    setSongsLoaded(songs);
+    setSongsLoaded(formatSongsLoadedForPlayer(songs));
+  }
+
+  // Flatten folders object into array
+  const formatSongsLoadedForPlayer = (songsByFolder) => {
+    const allSongs = [];
+    for (const [folderName, songs] of Object.entries(songsByFolder)) {
+      allSongs.push(...songs.map((song) => {
+        song.folderName = folderName
+        return song;
+      }))
+    }
+    return allSongs;
   }
 
   const Copyright = () => {
@@ -74,17 +103,90 @@ const App = (props) => {
     )
   }
 
-  // Flatten folders object into array
-  const formatSongsLoadedForPlayer = () => {
-    const allSongs = [];
-    for (const [folderName, songs] of Object.entries(songsLoaded)) {
-      allSongs.push(...songs.map((song) => {
-        song.folderName = folderName
-        return song;
-      }))
+  const nextSong = async () => {
+    let i = trackIndex;
+    i++;
+    trackRef.current.pause();
+    if (i === songsLoaded.length) {
+        i = 0;
     }
-    return allSongs;
+    handleChangeTrack(i);
   }
+
+  const previousSong = async () => {
+    let i = trackIndex;
+    i--;
+    trackRef.current.pause();
+    if (i < 0) {
+        i = songsLoaded.length - 1;
+    }
+    handleChangeTrack(i);
+  }
+
+  const handleChangeTrack = (trackIndex) => {
+    setTrackIndex(trackIndex);
+    setTrackLoaded(false);
+    setSrc('');
+  }
+
+  const playPause = () => {
+    if (!isPlaying) {
+        setIsPlaying(true);
+    } else {
+        setIsPlaying(false);
+    }
+  }
+
+  const handlePlayPause = async () => {
+    if (trackRef.current) {
+      const doPlayPause = () => {
+          if (isPlaying) {
+              trackRef.current.play();
+          } else {
+              trackRef.current.pause();
+          }
+      };
+      if (!trackRef.current.src.includes('blob') && songsLoaded.length) {
+          loadSong(songsLoaded[trackIndex]).then(doPlayPause);
+      } else if (trackLoaded) {
+          doPlayPause();
+      }
+    }
+  }
+
+  const handleSetVolume = (e, value) => {
+    const newVolume = value / 100;
+    trackRef.current.volume = newVolume;
+    setVolume(newVolume);
+  }
+
+  const loadSong = (file) => {
+    return new Promise((res, rej) => {
+        if (trackRef.current) {
+            getAccessToken(async (token) => {
+                const trackBlob = await fetchDriveFileBlob(file, token);
+                setSrc(URL.createObjectURL(trackBlob));
+                trackRef.current.onend = nextSong;
+                trackRef.current.volume = volume;
+                trackRef.current.load();
+                trackRef.current.oncanplay = () => {
+                    setTrackLoaded(true);
+                    res();
+                };
+            });
+        }
+    });
+  }
+
+  useEffect(() => {
+      handlePlayPause();
+  }, [isPlaying, trackIndex, trackRef]);
+
+  useEffect(() => {
+    if (trackRef.current) {
+      trackRef.current.src = '';
+    }
+  }, [songsLoaded])
 
   return (
     <Container maxWidth="sm">
@@ -98,10 +200,21 @@ const App = (props) => {
           <Grid item xs={16} style={{...componentDisplayStyle, ...headerFooterStyle}}>
             <Header />
           </Grid>
-          <Grid item xs={16} style={componentDisplayStyle}>
-            <Player songs={formatSongsLoadedForPlayer()} trackRef={trackRef} />
-            <Playlist />
-          </Grid>
+          {
+            songsLoaded.length > 0
+            &&
+            <Grid item xs={16} style={componentDisplayStyle}>
+              <audio src={src} ref={trackRef} />
+              <PlayerControls 
+                playPause={playPause}
+                nextSong={nextSong}
+                previousSong={previousSong}
+                volume={volume}
+                handleSetVolume={handleSetVolume}
+                isPlaying={isPlaying}
+               />
+            </Grid>      
+          }
           <Grid item xs={16} style={{...componentDisplayStyle, ...headerFooterStyle}}>
             <Copyright />
           </Grid>
---
+++ src/ui/components/playercontrols.js
@@ -0,0 +1,86 @@
+import {useRef, useState, useEffect} from 'react';
+
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Slider from '@mui/material/Slider';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
+import VolumeDown from '@mui/icons-material/VolumeDown';
+import VolumeUp from '@mui/icons-material/VolumeUp';
+import PauseOutlinedIcon from '@mui/icons-material/PauseOutlined';
+import PlayArrowOutlinedIcon from '@mui/icons-material/PlayArrowOutlined';
+import SkipNextOutlinedIcon from '@mui/icons-material/SkipNextOutlined';
+import SkipPreviousOutlinedIcon from '@mui/icons-material/SkipPreviousOutlined';
+
+import {playerButtonStyle} from '../styles/styles.js';
+
+const PlayerControls = (props) => {
+
+    const [isHovering, setIsHovering] = useState(false)
+
+    const handleMouseEnter = () => {
+        setIsHovering(true);
+    }
+
+    const handleMouseLeave = () => {
+        setIsHovering(false);
+    }
+
+    const handleSetVolume = (e) => {
+        props.setVolume(e.target.value);
+    }
+
+    return (
+        <div>
+            <ButtonGroup variant="contained" aria-label="outlined button group">
+                <Button 
+                    className="prev-track" 
+                    onClick={props.previousSong} 
+                    style={playerButtonStyle(isHovering)}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                >
+                  <SkipPreviousOutlinedIcon />
+                </Button>
+                <Button 
+                    className="playpause-track" 
+                    onClick={props.playPause} 
+                    style={playerButtonStyle(isHovering)}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                >
+                    {
+                        props.isPlaying
+                        ?
+                        <PauseOutlinedIcon />
+                        :
+                        <PlayArrowOutlinedIcon />
+                    }
+                </Button>
+                <Button 
+                    className="next-track" 
+                    onClick={props.nextSong} 
+                    style={playerButtonStyle(isHovering)}
+                    onMouseEnter={handleMouseEnter}
+                    onMouseLeave={handleMouseLeave}
+                >
+                  <SkipNextOutlinedIcon />
+                </Button>
+            </ButtonGroup>
+            { /* ToDo: implement Controlled Slider correctly for volume change */ }
+            <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+                <VolumeDown />
+                    <Slider 
+                      aria-label="Volume" 
+                      value={props.volume} 
+                      onChange={props.handleSetVolume} 
+                      style={{color: 'black'}} 
+                    />
+                <VolumeUp />
+            </Stack>  
+        </div>
+    )
+}
+
+export default PlayerControls;
