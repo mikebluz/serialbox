9c3c04613b9a6ff181b85319da6c17bc1c44eb94
Mike Luz <mikeluz@Mikes-MacBook-Pro.local>
2023-06-15 14:05:35 -0400

    fix: cert fix

--- src/server/server.js
+++ src/server/server.js
@@ -17,8 +17,8 @@ let httpOpts;
 if (process.env.PROD) {
   httpsOpts = {
     ca: fs.readFileSync("../../../../../etc/pki/tls/certs/ca-bundle.crt"),
-    key: fs.readFileSync("../../../../../etc/pki/tls/private/api.serialboxmusic.com.key"),
-    cert: fs.readFileSync("../../../../../etc/pki/tls/certs/api.serialboxmusic.com.crt")
+    key: fs.readFileSync("../../../../../etc/letsencrypt/live/api.serialboxmusic.com/privkey.pem"),
+    cert: fs.readFileSync("../../../../../etc/letsencrypt/live/api.serialboxmusic.com/fullchain.pem")
   };
 }
 
